88e2a067627745e46c840b2404fdbb3c34245dca
Hadi Zhang <hadizhang@berkeley.edu>
2017-08-11 07:29:20 +0000

    Added functional tests to team_controller

--- spec/controllers/team_controller_spec.rb
+++ spec/controllers/team_controller_spec.rb
@@ -1,15 +1,17 @@
 RSpec.describe TeamController, type: :controller do
   describe "next_rec" do
     it "assigns @suggested_team" do
+      get(:show, {'id' => "1"}, {'user_id' => 1})
       xhr :get, :next_rec
-      expect(response).to redirect_to("http://test.host/login")
+      expect(response).to render_template(partial: "_pane")
     end
   end
 
   describe "prev_rec" do
     it "assigns @suggested_team" do
-      xhr :get, :next_rec
-      expect(response).to redirect_to("http://test.host/login")
+      get(:show, {'id' => "1"}, {'user_id' => 1})
+      xhr :get, :prev_rec
+      expect(response).to render_template(partial: "_pane")
     end
   end
 end
